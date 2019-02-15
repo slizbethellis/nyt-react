d8b71c21e310850bed1c2a7587e9af63e5de2326
slizbethellis <slizbethellis@gmail.com>
2019-02-15 16:43:07 -0600

    ran build to replace missing build files

---
+++ client/build/precache-manifest.3541309be65724fd7366adb5a0ea6b6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "228ec1f0725a939f2c74",
+    "url": "/static/css/main.4f295ce1.chunk.css"
+  },
+  {
+    "revision": "228ec1f0725a939f2c74",
+    "url": "/static/js/main.228ec1f0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dfffbf2bf36ddca1f7b5",
+    "url": "/static/js/2.dfffbf2b.chunk.js"
+  },
+  {
+    "revision": "752accb288493bbff4d53758e59d6689",
+    "url": "/index.html"
+  }
+];
